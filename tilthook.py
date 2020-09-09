e90d086fdca225a693020fe865e0ffa362323ccf
Lin Meyer <lin@linmeyer.net>
2020-09-09 05:24:06 +0100

    Added celcius

--- tilthook.py
+++ tilthook.py
@@ -3,6 +3,14 @@ import time
 from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement, parse_packet
 from prometheus_client import start_http_server, Counter, Gauge
 
+class TiltStatus:
+
+    def __init__(self, color, temp_f, gravity):
+        self.color = color
+        self.temp_f = temp_f
+        self.temp_c = get_celcius(temp_f)
+        self.gravity = gravity
+
 # statics
 color_map = {
         "a495bb20-c5b1-4b44-b512-1370f02d74de": "Green",
@@ -15,7 +23,8 @@ color_map = {
     }
 
 counter_beacons_received = Counter('beacons_received', 'Number of beacons received', ['color'])
-gauge_temperature_fahrenheit = Gauge('emperature_fahrenheit', 'Temperature in fahrenheit', ['color'])
+gauge_temperature_fahrenheit = Gauge('temperature_fahrenheit', 'Temperature in fahrenheit', ['color'])
+gauge_temperature_celcius = Gauge('temperature_celcius', 'Temperature in celcius', ['color'])
 gauge_gravity = Gauge('gravity', 'Gravity of the beer', ['color'])
 #########
 
@@ -26,27 +35,30 @@ def callback(bt_addr, rssi, packet, additional_info):
         # iBeacon packets have major/minor attributes with data
         # major = degrees in F (int)
         # minor = gravity (int) - needs to be converted to float (e.g. 1035 -> 1.035)
-        gravity = get_decimal_gravity(packet.minor)
-        degrees_f = packet.major
-        tilt_metrics(color, degrees_f, gravity)
-        tilt_hooks(color, degrees_f, gravity)
+        tilt_status = TiltStatus(color, packet.major, get_decimal_gravity(packet.minor))
+        tilt_metrics(tilt_status)
+        tilt_hooks(tilt_status)
 
-def tilt_metrics(color, temp_f, gravity):
-    counter_beacons_received.labels(color=color).inc()
-    gauge_temperature_fahrenheit.labels(color=color).set(temp_f)
-    gauge_gravity.labels(color=color).set(gravity)
+def tilt_metrics(tilt_status: TiltStatus):
+    counter_beacons_received.labels(color=tilt_status.color).inc()
+    gauge_temperature_fahrenheit.labels(color=tilt_status.color).set(tilt_status.temp_f)
+    gauge_temperature_celcius.labels(color=tilt_status.color).set(tilt_status.temp_c)
+    gauge_gravity.labels(color=tilt_status.color).set(tilt_status.gravity)
 
-def tilt_hooks(color, temp_f, gravity):
+def tilt_hooks(tilt_status: TiltStatus):
     print("-------------------------------------------")
-    print("Broadcoast for device: {}".format(color))
-    print("Temperature: {}F".format(temp_f))
-    print("Gravity: {}".format(gravity))
+    print("Broadcoast for device: {}".format(tilt_status.color))
+    print("Temperature: {}f ({}c)".format(tilt_status.temp_f, tilt_status.temp_c))
+    print("Gravity: {}".format(tilt_status.gravity))
 
 def get_decimal_gravity(gravity):
     # gravity will be an int like 1035
     # turn into decimal, like 1.035
     return gravity * .001
-    
+
+def get_celcius(temp_f):
+    return round((temp_f - 32) * 5.0/9.0)
+
 
 # scan for all iBeacon advertisements from beacons with certain properties:
 # - uuid
