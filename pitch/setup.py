9ac863aeb401a8dfbbb7bef9ffd9a36a2d44a681
Lin Meyer <lin@linmeyer.net>
2020-09-15 21:40:57 -0500

    Added package setup workflow

---
+++ pitch/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tilt-pitch",
+    version="0.0.1",
+    author="Lin Meyer",
+    author_email="lin@linmeyer.net",
+    description="Simple replacement for the Tilt Hydrometer mobile apps and TiltPi with lots of features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/linjmeyer/tilt-pitch/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.7',
+)
